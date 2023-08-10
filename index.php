c6c5affaa73dd319c6a607c5b2363dd4541bef9d
Merlin Migan <merlin.migan@gmail.com>
2023-08-10 15:00:47 +0200

    Ajouter des range sliders pour les filtres de tri des véhicules

--- index.php
+++ index.php
@@ -20,6 +20,54 @@
         </form>
     </div>
 
+    <div class="container mt-2">
+        <form class="d-flex justify-content-around">
+
+            <div class="">
+                <label class="d-inline-block">Kilométrage</label>
+                <input 
+                    type="range"
+                    min="0"
+                    max="100"
+                    step="1"
+                    value="0"
+                    class="d-block"
+                />
+                <span class="">177 220 km - 267 220 km</span>
+                <button class="reset-btn">Réinitialiser</button>
+            </div>
+
+            <div class="">
+                <label class="d-inline-block">Prix</label>
+                <input 
+                    type="range"
+                    min="0"
+                    max="100"
+                    step="1"
+                    value="0"
+                    class="d-block mr-5"
+                />
+                <span class="ml-5">177 220 km - 267 220 km</span>
+                <button class="reset-btn">Réinitialiser</button>
+            </div>
+
+            <div class="">
+                <label class="d-inline-block">Année</label>
+                <input 
+                    type="range"
+                    min="0"
+                    max="100"
+                    step="1"
+                    value="0"
+                    class="d-block mr-5"
+                />
+                <span class="ml-5">177 220 km - 267 220 km</span>
+                <button class="reset-btn">Réinitialiser</button>
+            </div>
+
+        </form>
+    </div>
+
     <div id="cars-container" class="container mt-4 mb-4 pt-4">
 
         <div class="row row-cols-1 row-cols-md-3 g-4 pb-4">
@@ -51,7 +99,7 @@
             <div class="col">
                 <div class="card h-100 shadow custom-card">
                     <span class="btn btn-dark" id="price-tag">10 000 €</span>
-                    <img src="img/v3.jpg" alt="" class="card-img-top w-100 custom-bg">
+                    <img src="img/v4.jpg" alt="" class="card-img-top w-100 custom-bg">
                     <div class="card-body">
                         <h4 class="card-title">Porsche Elantra 2016 120 CH</h4>
                         <ul class="card-text list-unstyled">
@@ -75,7 +123,7 @@
             <div class="col">
                 <div class="card h-100 shadow custom-card">
                     <span class="btn btn-dark" id="price-tag">140 441 €</span>
-                    <img src="img/v3.jpg" alt="" class="card-img-top w-100 custom-bg">
+                    <img src="img/v5.jpg" alt="" class="card-img-top w-100 custom-bg">
                     <div class="card-body">
                         <h4 class="card-title">Porsche Elantra 2016 120 CH</h4>
                         <ul class="card-text list-unstyled">
@@ -101,7 +149,7 @@
                     <span class="btn btn-dark" id="price-tag">140 441 €</span>
                     <img src="img/v3.jpg" alt="" class="card-img-top w-100 custom-bg">
                     <div class="card-body">
-                        <h4 class="card-title">Porsche Elantra 2016 120 CH</h4>
+                        <h4 class="card-title">Mitsubishi Elantra 2016 120 CH</h4>
                         <ul class="card-text list-unstyled">
                             <li><span>Année : </span>2016</li>
                             <li><span>Type de moteur : </span>Diesel</li>
