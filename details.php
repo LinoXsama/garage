ea5f684993dcf21bc86537bf31a5ccf0a48533aa
Merlin Migan <merlin.migan@gmail.com>
2023-09-10 20:15:10 +0200

    Modification du code PHP de soumission du formulaire

--- details.php
+++ details.php
@@ -19,6 +19,13 @@
 
 <main>
 
+    <?php
+        if(isset($_SESSION['car_to_detail']))
+        {
+
+        }
+    ?>
+
     <h4 class="text-center mt-3">Galérie d'images</h4>
 
     <div class="gallery card shadow">
@@ -38,12 +45,12 @@
 
     </div>
 
-    <h4 class="text-center mb-3"> Détails du véhicule</h4>
+    <h4 class="text-center mb-3">Détails du véhicule</h4>
 
     <div class="container">
         <div class="card ml-3 mr-3 mb-5">
     
-        <h4 class="card-header">Opel Astra 2016 100 CH</h4>
+        <h4 class="card-header">Opel Astra 2016 - 100 CH</h4>
 
         <div class="card-body">
 
@@ -84,52 +91,51 @@
         </div>
     </div>
     
-        <?php
-            if(isset($_POST['CONTACT_FORM']))
+    <?php
+        if(isset($_POST['CONTACT_FORM']))
+        {
+            $_SESSION['name'] = htmlspecialchars($_POST['NAME']);
+            $_SESSION['email'] = htmlspecialchars($_POST['EMAIL']);
+            $_SESSION['phone'] = htmlspecialchars($_POST['PHONE']);
+            $_SESSION['comment'] = htmlspecialchars($_POST['COMMENT']);
+
+            if(!empty($_SESSION['name']) & !empty($_SESSION['email']) & !empty($_SESSION['phone']) & !empty($_SESSION['comment']))
             {
-                session_start();
-
-                $table = 'contacts';
-
-                $_SESSION['name'] = htmlspecialchars($_POST['NAME']);
-                $_SESSION['email'] = htmlspecialchars($_POST['EMAIL']);
-                $_SESSION['phone'] = htmlspecialchars($_POST['PHONE']);
-                $_SESSION['comment'] = htmlspecialchars($_POST['COMMENT']);
-
-                insert($table, 'name', $_SESSION['name'], 'email', $_SESSION['email'], 'phone', $_SESSION['phone'], 'msg', $_SESSION['comment']);
+                insert('contacts', 'name', $_SESSION['name'], 'email', $_SESSION['email'], 'phone', $_SESSION['phone'], 'msg', $_SESSION['comment']);
             }
-        ?>
+        }
+    ?>
 
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-8">
-                    <div class="card shadow">
-                        <div class="card-header">
-                            <h5 class="pt-2 text-align text-center">Contactez-nous</h5>
-                        </div>
-                        <div class="card-body">
+    <div class="container mb-5">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card shadow">
+                    <div class="card-header">
+                        <h5 class="pt-2 text-align text-center">Contactez-nous</h5>
+                    </div>
+                    <div class="card-body">
 
-                            <form action="contact.php" method="POST">
+                        <form action="contact.php" method="POST">
 
-                                <div class="form-group my-3 mx-3" >
-                                    <input type="text" name="NAME" placeholder="Vos nom et prénom" class="form-control my-3">
-                                    <input type="text" name="EMAIL" placeholder="Votre adresse email" class="form-control mb-3">
-                                    <input type="text" name="PHONE" placeholder="Votre numéro de téléphone" class="form-control">
+                            <div class="form-group my-3 mx-3" >
+                                <input type="text" name="NAME" placeholder="Vos nom et prénom" class="form-control my-3">
+                                <input type="text" name="EMAIL" placeholder="Votre adresse email" class="form-control mb-3">
+                                <input type="text" name="PHONE" placeholder="Votre numéro de téléphone" class="form-control">
 
-                                    <textarea rows="4" name="COMMENT" placeholder="Rédigez votre message ici..." class="form-control my-3"></textarea>
-                                    
-                                    <div class="text-center my-4">
-                                        <button type="submit" name="CONTACT_FORM" value="contact_form" class="btn btn-primary">SOUMETTRE</button>
-                                    </div>
+                                <textarea rows="4" name="COMMENT" placeholder="Rédigez votre message ici..." class="form-control my-3"></textarea>
+                                
+                                <div class="text-center my-4">
+                                    <button type="submit" name="CONTACT_FORM" value="contact_form" class="btn btn-primary">SOUMETTRE</button>
                                 </div>
+                            </div>
 
-                            </form>
+                        </form>
 
-                        </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
     
 
 </main>
