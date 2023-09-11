7c06f493834ffce07e9460fab5385daeb7a9705b
Merlin Migan <merlin.migan@gmail.com>
2023-09-11 15:44:53 +0200

    utilisation de mysqli_real_escape_string pour améliorer la sécurité des données

--- details_transition.php
+++ details_transition.php
@@ -4,7 +4,7 @@
     require_once 'config/db_connect.php';
 
     $_SESSION['CAR_TO_DETAIL'] = $conn->real_escape_string($_GET['car_id']);
-
+    
     header('Location: details.php');
     exit;
 ?>
