b06863bf3536e3a4cf527d66b1b836d607578f3c
Merlin Migan <merlin.migan@gmail.com>
2023-09-11 15:41:51 +0200

    traitement_formulaire.php peut à présent traiter les requêtes provenant aussi bien de details.php que de contact.php

--- traitement_formulaire.php
+++ traitement_formulaire.php
@@ -5,26 +5,18 @@ session_start();
 require_once 'functions.php';
 require_once 'config/db_connect.php';
 
-if(isset($_POST['CONTACT_FORM'])) 
+
+if(isset($_POST['CONTACT_FORM']) & $_POST['CONTACT_FORM'] === 'details.php') 
 {
-    $_SESSION['NAME'] = htmlspecialchars($conn->real_escape_string($_SESSION['NAME']));
-    $_SESSION['EMAIL'] = htmlspecialchars($conn->real_escape_string($_SESSION['EMAIL']));
-    $_SESSION['PHONE'] = htmlspecialchars($conn->real_escape_string($_SESSION['PHONE']));
+    $_SESSION['NAME'] = htmlspecialchars($conn->real_escape_string($_POST['NAME']));
+    $_SESSION['EMAIL'] = htmlspecialchars($conn->real_escape_string($_POST['EMAIL']));
+    $_SESSION['PHONE'] = htmlspecialchars($conn->real_escape_string($_POST['PHONE']));
     $_SESSION['COMMENT'] = $conn->real_escape_string($_POST['COMMENT']);
 
     if(!empty($_SESSION['NAME']) & !empty($_SESSION['EMAIL']) & !empty($_SESSION['PHONE']) & !empty($_SESSION['COMMENT']))
     {
-        if(isset($_SESSION['CAR_TO_DETAIL']))
-        {
-            $car_id = $_SESSION['CAR_TO_DETAIL'];
-        }
-        else
-        {
-            $car_id = '';
-        }
-
-        $query_status = insert('contacts', 'name', $_SESSION['NAME'], 'email', $_SESSION['EMAIL'], 'phone', $_SESSION['PHONE'], 'msg', $_SESSION['COMMENT'], 'car_id', $car_id);
-
+        $query_status = insert('contacts', 'name', $_SESSION['NAME'], 'email', $_SESSION['EMAIL'], 'phone', $_SESSION['PHONE'], 'msg', $_SESSION['COMMENT'], 'car_id', $_SESSION['CAR_TO_DETAIL']);
+        
         if($query_status)
         {
             $_SESSION['msg'] = "Votre demande d'informations a bien été prise en compte !";
@@ -39,6 +31,53 @@ if(isset($_POST['CONTACT_FORM']))
             $_SESSION['alert_type'] = 'danger';
 
             header('Location: details.php');
+            exit;
+        }
+    }
+    else 
+    {
+        $_SESSION['msg'] = "Vous devez remplir tous les champs du formulaire afin de soumettre votre demande d'informations !";
+        $_SESSION['alert_type'] = 'warning';
+
+        header('Location: details.php');
+        exit;
+    }
+}
+else if(isset($_POST['CONTACT_FORM']) & $_POST['CONTACT_FORM'] === 'contact.php')
+{
+    {
+        $_SESSION['NAME'] = htmlspecialchars($conn->real_escape_string($_POST['NAME']));
+        $_SESSION['EMAIL'] = htmlspecialchars($conn->real_escape_string($_POST['EMAIL']));
+        $_SESSION['PHONE'] = htmlspecialchars($conn->real_escape_string($_POST['PHONE']));
+        $_SESSION['COMMENT'] = $conn->real_escape_string($_POST['COMMENT']);
+    
+        if(!empty($_SESSION['NAME']) & !empty($_SESSION['EMAIL']) & !empty($_SESSION['PHONE']) & !empty($_SESSION['COMMENT']))
+        {
+            $query_status = insert('contacts', 'name', $_SESSION['NAME'], 'email', $_SESSION['EMAIL'], 'phone', $_SESSION['PHONE'], 'msg', $_SESSION['COMMENT']);
+            
+            if($query_status)
+            {
+                $_SESSION['msg'] = "Votre demande d'informations a bien été prise en compte !";
+                $_SESSION['alert_type'] = 'success';
+    
+                header('Location: contact.php');
+                exit;
+            }
+            else
+            {
+                $_SESSION['msg'] = "Une erreur est survenue. Votre demande n'a pu être prise en compte !";
+                $_SESSION['alert_type'] = 'danger';
+    
+                header('Location: contact.php');
+                exit;
+            }
+        }
+        else 
+        {
+            $_SESSION['msg'] = "Vous devez remplir tous les champs du formulaire afin de soumettre votre demande d'informations !";
+            $_SESSION['alert_type'] = 'warning';
+    
+            header('Location: contact.php');
             exit;
         }
     }
