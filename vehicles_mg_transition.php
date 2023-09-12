648ecd3a3f3f9ff3e06be9809847935f48e4a4b9
Merlin Migan <merlin.migan@gmail.com>
2023-09-12 16:58:57 +0200

    Cette page établit une transition entre cars_mg.php et delete-cars.php

---
+++ vehicles_mg_transition.php
@@ -0,0 +1,10 @@
+<?php
+    session_start();
+
+    require_once 'config/db_connect.php';
+
+    $_SESSION['CAR_TO_DETAIL'] = $conn->real_escape_string($_GET['id']);
+    
+    header('Location: details.php');
+    exit;
+?>
