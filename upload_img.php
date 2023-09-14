7e9f9b23cf839cd131a034ec86075681929341ab
Merlin Migan <merlin.migan@gmail.com>
2023-09-14 18:23:34 +0200

    Modification de la page upload_img.php : les images doivent pouvoir être ajoutées séparément

--- upload_img.php
+++ upload_img.php
@@ -6,38 +6,7 @@
     require_once 'functions.php';
 ?>
 
-<main>
-
-    <?php
-        $car = select('cars', 'cars_id', $_SESSION['CAR_ID']);
-
-        $row = mysqli_fetch_assoc($car);
-    ?>
-    
-    <h4 class="text-center mt-3">Images actuelles du véhicule N° <?= "{$row['cars_id']} : {$row['cars_brand']} {$row['cars_model']} de {$row['cars_owner']}"; ?></h4>
-
-    <div class="gallery">
-        <div class="container-lg">
-
-            <div class="row gy-4 row-cols-1 -row-cols-sm-2 row-cols-md-3">
-                <div class="col">
-                    <img src="<?= $row['cars_main_img']; ?>" alt="<?= $row['cars_alt_text']; ?>" class="cars-gallery-item shadow bg-warning" />
-                </div>
-                <div class="col">
-                    <img src="<?= $row['cars_gallery_img1']; ?>" alt="<?= $row['cars_alt_text_img1']; ?>" class="cars-gallery-item shadow" />
-                </div>
-                <div class="col">
-                    <img src="<?= $row['cars_gallery_img2']; ?>" alt="<?= $row['cars_alt_text_img2']; ?>" class="cars-gallery-item shadow" />
-                </div>
-                <div class="col">
-                    <img src="<?= $row['cars_gallery_img3']; ?>" alt="<?= $row['cars_alt_text_img3']; ?>" class="cars-gallery-item shadow" />
-                </div>
-            </div>
-        </div>
-
-    </div>
-
-    <?php
+<?php
         if(isset($_SESSION['IMG_SUCCESS_ALL']))
         {
             echo
@@ -92,9 +61,41 @@
 
             unset($_SESSION['SUCCESS']);
         }
-    ?>
+?>
 
-    <div class="py-5">
+<main>
+
+    <?php
+        $car = select('cars', 'cars_id', $_SESSION['CAR_ID']);
+
+        $row = mysqli_fetch_assoc($car);
+    ?>
+    
+    <h4 class="text-center mt-3">Images actuelles du véhicule N° <?= "{$row['cars_id']} : {$row['cars_brand']} {$row['cars_model']} de {$row['cars_owner']}"; ?></h4>
+
+    <div class="gallery">
+        <div class="container-lg">
+
+            <div class="row gy-4 row-cols-1 -row-cols-sm-2 row-cols-md-3">
+                <div class="col">
+                    <img src="<?= $row['cars_main_img']; ?>" alt="<?= $row['cars_alt_text']; ?>" class="cars-gallery-item shadow bg-warning" />
+                </div>
+                <div class="col">
+                    <img src="<?= $row['cars_gallery_img1']; ?>" alt="<?= $row['cars_alt_text_img1']; ?>" class="cars-gallery-item shadow" />
+                </div>
+                <div class="col">
+                    <img src="<?= $row['cars_gallery_img2']; ?>" alt="<?= $row['cars_alt_text_img2']; ?>" class="cars-gallery-item shadow" />
+                </div>
+                <div class="col">
+                    <img src="<?= $row['cars_gallery_img3']; ?>" alt="<?= $row['cars_alt_text_img3']; ?>" class="cars-gallery-item shadow" />
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+    <!-- <div class="py-5">
             <div class="container">
                 <div class="row justify-content-center">
                     <div class="col-md-8">
@@ -119,6 +120,106 @@
                                     <input class="form-control" type="file" name="CARS_IMG3">
 
                                     <button class="btn btn-primary mt-4" type="submit" name="UPLOAD">ENREGISTRER</button>
+
+                                </form>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div> -->
+
+    <div class="mt-5 mb-3">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-8">
+                        <div class="card shadow">
+                            <div class="card-header">
+                                <h5 class="pt-2 text-align text-center">Ajouter l'image de la miniature</h5>
+                            </div>
+                            <div class="card-body">
+
+                                <form action="formulaire_upload_img.php" method="POST" enctype="multipart/form-data">
+
+                                    <input class="form-control" type="file" name="CARS_THUMBNAIL_IMG">
+
+                                    <button class="btn btn-primary mt-4" type="submit" name="THUMBNAIL">ENREGISTRER</button>
+
+                                </form>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+
+    <div class="mb-3">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-8">
+                        <div class="card shadow">
+                            <div class="card-header">
+                                <h5 class="pt-2 text-align text-center">Galérie - Image 1</h5>
+                            </div>
+                            <div class="card-body">
+
+                                <form action="formulaire_upload_img.php" method="POST" enctype="multipart/form-data">
+
+                                    <input class="form-control" type="file" name="CARS_IMG1">
+
+                                    <button class="btn btn-primary mt-4" type="submit" name="IMG1">ENREGISTRER</button>
+
+                                </form>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+
+    <div class="mb-3">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-8">
+                        <div class="card shadow">
+                            <div class="card-header">
+                                <h5 class="pt-2 text-align text-center">Galérie - Image 2</h5>
+                            </div>
+                            <div class="card-body">
+
+                                <form action="formulaire_upload_img.php" method="POST" enctype="multipart/form-data">
+
+                                    <input class="form-control" type="file" name="CARS_IMG2">
+
+                                    <button class="btn btn-primary mt-4" type="submit" name="IMG2">ENREGISTRER</button>
+
+                                </form>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+
+    <div class="mb-5">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-8">
+                        <div class="card shadow">
+                            <div class="card-header">
+                                <h5 class="pt-2 text-align text-center">Galérie - Image 3</h5>
+                            </div>
+                            <div class="card-body">
+
+                                <form action="formulaire_upload_img.php" method="POST" enctype="multipart/form-data">
+
+                                    <input class="form-control" type="file" name="CARS_IMG3">
+
+                                    <button class="btn btn-primary mt-4" type="submit" name="IMG3">ENREGISTRER</button>
 
                                 </form>
 
