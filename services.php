21caf2ea9f6344a2e53077d4e7df978305885b7e
Merlin Migan <merlin.migan@gmail.com>
2023-09-18 04:59:04 +0200

    Création de services.php + gestion du layout, de services_add.php

--- services.php
+++ services.php
@@ -1,19 +1,64 @@
 <?php
-    $page_title = 'Nos véhicules';
+    $page_title = 'Nos services';
 ?>
 
 <?php
     require_once 'templates/header.php';
     require_once 'templates/navbar.php';
-    // require_once 'functions.php';
 ?>
 
-<main>
+<main style="height: 80%; overflow-y: auto;">
 
-    <h1>Réparation de véhicule</h1>
-    <h1>Vente de véhicules d'occasion</h1>
-    <h1>Peinture de véhicule</h1>
-    <h1>Travaux de carrosserie</h1>
+    <div class="container mt-5">
+        <div class="row">
+
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Entretien général</h5>
+                        <p class="card-text">Nous offrons des services d'entretien régulier pour maintenir votre véhicule en bon état.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="card">                   
+                    <div class="card-body">
+                        <h5 class="card-title">Réparations mécaniques</h5>
+                        <p class="card-text">Notre équipe de mécaniciens expérimentés est prête à résoudre tous les problèmes mécaniques de votre véhicule.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="card">                 
+                    <div class="card-body">
+                        <h5 class="card-title">Carrosserie et peinture</h5>
+                        <p class="card-text">Nous proposons des services de réparation de carrosserie et de peinture pour redonner de l'éclat à votre véhicule.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="card">                 
+                    <div class="card-body">
+                        <h5 class="card-title">Remorquage et dépannage</h5>
+                        <p class="card-text">Réponse rapide pour les pannes sur la route, nous vous ramenons sur la route en un clin d'œil.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Rénovation d'intérieur de véhicule</h5>
+                        <p class="card-text">Donnez une nouvelle vie à l'intérieur de votre véhicule avec nos services de rénovation complète.</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
 
 </main>
 
---
+++ services_add.php
@@ -0,0 +1,81 @@
+<?php
+
+    session_start();
+
+    // if(!isset($_SESSION['user_id']))
+    // {
+    //     header('Location: login.php');
+    //     exit;
+    // }
+    // else
+    // {
+?>
+
+<?php
+    require_once 'templates/header.php';
+    require_once 'templates/navbar.php';
+?>
+
+<main style="height: 90%;" class="container">
+    <!-- GESTION DES MESSAGES D'ERREURS - START -->
+        <?php
+            if(isset($_SESSION['ERRORS_ADD_SERVICES'])) 
+            {
+                echo '<div class="container mt-3">';
+                    echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">';
+
+                echo '<ul>';
+                foreach ($_SESSION['ERRORS_ADD_SERVICES'] as $error) 
+                {
+                    echo '<li>' .$error. '</li>';
+                }
+                echo '<ul>';
+
+                    echo '<button type="button" class="btn-close" data-dismiss="alert" aria-label="Close"></button>';
+                    echo '</div>';
+                echo '</div>';
+                
+                unset($_SESSION['ERRORS_ADD_SERVICES']); 
+            }
+        ?>
+    <!-- GESTION DES MESSAGES D'ERREURS - END -->
+
+    <div class="py-5">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card shadow">
+                        <div class="card-header">
+                            <h5 class="pt-2 text-align text-center">Ajouter un nouveau service</h5>
+                        </div>
+                        <div class="card-body">
+
+                        <form action="services_formulaire.php" method="POST" class="mt-3 mb-3">
+
+                            <label class="form-label responsive-font">Nom du service</label>
+                            <input class="form-control" type="text" name="SERVICE_NAME" placeholder="Ex : Réparation">
+                            
+                            <label class="form-label responsive-font">Description du service</label>
+                            <input class="form-control" type="text" name="SERVICE_DESCRIPTION" placeholder="Ex : Nous réparons vos...">
+
+                            <div class="mt-4">
+                                <button type="submit" name="ADD_SERVICES" class="btn btn-success responsive-font">ENREGISTRER</button>
+                                <a href="" class="btn btn-danger ctm-btn responsive-font">RETOUR</a>
+                            </div>
+
+                        </form>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</main>
+
+<?php include('templates/footer.php'); ?>
+
+<?php
+    // }
+?>
