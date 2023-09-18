ba0ab216798a9f62560ccbab13b35f58648d997e
Merlin Migan <merlin.migan@gmail.com>
2023-09-18 15:01:17 +0200

    Modfication du layout de la page d'accueil index.php

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+    $page_title = "Accueil";
+
+    require_once 'templates/header.php';
+    require_once 'templates/navbar.php';
+?>
+
+<main class="main-index">
+
+    <div class="container mt-4 mb-4">
+    
+        <h1 class="text-center pb-2">Bienvenue au Garage V.Parrot</h1>
+
+        <div class="container px-5 d-flex justify-content-center">
+    <div>
+        <p>
+            Au Garage V.Parrot, nous sommes fiers de vous offrir des services automobiles exceptionnels depuis de nombreuses années. Notre équipe de professionnels expérimentés est dédiée à fournir des solutions de qualité pour vos besoins automobiles.
+        </p>
+
+        <p>
+            Nous nous engageons à garantir la performance et la sécurité de votre véhicule, qu'il s'agisse d'entretien général, de réparations mécaniques, de carrosserie et peinture, de remorquage et dépannage, ou de la rénovation de l'intérieur de votre véhicule. Chez Garage V.Parrot, votre satisfaction est notre priorité.
+        </p>
+
+        <p>
+            Notre Engagement envers Vous
+            <br>
+            Nous considérons chaque client comme un partenaire, et nous nous efforçons de créer des relations de confiance à long terme. Votre véhicule est précieux, et nous nous engageons à en prendre soin comme s'il s'agissait du nôtre.
+        </p>
+
+        <p>
+            Pour toute question ou pour planifier un rendez-vous, n'hésitez pas à nous contacter. Nous sommes là pour répondre à vos besoins automobiles.
+        </p>
+
+        <p>
+            Merci de nous faire confiance pour vos besoins automobiles. Nous avons hâte de vous servir.
+        </p>
+
+        <!-- Texte étendu -->
+        <p>
+            À Garage V.Parrot, nous sommes résolus à fournir des services exceptionnels à nos clients. Nos années d'expérience et notre dévouement à la qualité nous permettent de vous offrir un éventail de services pour répondre à tous vos besoins automobiles. Qu'il s'agisse de l'entretien général de votre véhicule, de réparations mécaniques, de travaux de carrosserie et de peinture, de services de remorquage et de dépannage, ou même de la rénovation de l'intérieur de votre véhicule, nous sommes là pour vous servir.
+        </p>
+    </div>
+</div>
+
+
+
+</main>
+
+<?php require_once 'templates/footer.php'; ?>
